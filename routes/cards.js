1e729420cc5e7d09e3cc6a4b768c025ae50cf371
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-25 14:47:42 -0300

    refactor: adiciona fs e path para acessar e manipular arquivos de dados JSON

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
-const cards = require("../data/cards.json");
+const fs = require("fs");
+const path = require("path");
+
+const cards = path.join(__dirname, "../data/cards.json");
 
 router.get("/", (req, res) => {
-  res.send(cards);
+  fs.readFile(cards, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(data);
+  });
 });
 
 module.exports = router;
