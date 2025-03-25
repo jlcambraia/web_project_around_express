762d9c3de02e00366cd777db5b5a0cfeee5b3e5a
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-25 14:09:38 -0300

    feat: importa e usa rota dos cards e users

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const usersRouter = require("./routes/users");
+const cardRouter = require("./routes/cards");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use("/users", usersRouter);
+app.use("/cards", cardRouter);
 
 app.listen(PORT, () => {
   console.log(`O aplicativo está escutando na porta ${PORT}`);
