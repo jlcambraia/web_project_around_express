cf6b76417cc0e45d1f9b3df524673850d2f79d56
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-25 15:21:42 -0300

    feat: adiciona middleware para lidar com erro 500

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const app = express();
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
 
-app.use((req, res) => {
+const handlePageNotFound = (req, res, next) => {
   res.status(404).json({ message: "A solicitação não foi encontrada" });
-});
+};
+
+const handleServerError = (err, req, res, next) => {
+  res.status(500).json({ message: "Ocorreu um erro no servidor" });
+};
+
+app.use(handlePageNotFound);
+app.use(handleServerError);
 
 app.listen(PORT, () => {
   console.log(`O aplicativo está escutando na porta ${PORT}`);
