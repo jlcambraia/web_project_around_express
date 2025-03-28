ebde08f90e5d386a8db278fcee3aabf939c892a5
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-03-28 10:15:18 -0300

    feat: importa e conecta mongoose

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const usersRouter = require("./routes/users");
 const cardRouter = require("./routes/cards");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mydb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
