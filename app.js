47742efe2649fb32eb7919c9f900d9046f8230da
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 12:13:57 -0300

    feat: cria userSchema

--- app.js
+++ app.js
@@ -13,6 +13,30 @@ mongoose.connect("mongodb://localhost:27017/aroundb", {
   useFindAndModify: false,
 });
 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght: 30,
+  },
+  avatar: {
+    type: String,
+    required: true,
+    validator: (v) => {
+      return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+    },
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
+
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
 
