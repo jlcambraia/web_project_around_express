850532340fe2e766efd36fb7b84f5166c51a0e65
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 12:58:38 -0300

    feat: cria controller para rotas do user

---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res, next) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => next(err));
+};
+
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => next(err));
+};
+
+module.exports.createUser = (req, res, next) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => next(err));
+};
