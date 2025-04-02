573d2b9c72baf976f1e80cbeb78b98f7a83d9c20
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 12:14:33 -0300

    feat: cria cardSchema

--- app.js
+++ app.js
@@ -37,6 +37,38 @@ const userSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("user", userSchema);
 
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlenght30,
+  },
+  link: {
+    type: String,
+    required: true,
+    validator: (v) => {
+      return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "user",
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("card", cardSchema);
+
 app.use("/users", usersRouter);
 app.use("/cards", cardRouter);
 
