81ecd992d73417f71af4dbc4159b8d6c64423cae
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 14:07:14 -0300

    feat: adiciona app.use(express.urlencoded({ extended: true })); para correto recebimento dos dados

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://127.0.0.1:27017/aroundb", {
   useNewUrlParser: true,
