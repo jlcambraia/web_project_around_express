ba159b219e73ff43c7f410584d6b40ea0c13e76c
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 12:58:22 -0300

    refactor: transfere rotas para seu controller

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,10 @@
 const router = require("express").Router();
-const fs = require("fs");
-const path = require("path");
+const { getUsers, getUser, createUser } = require("../controllers/users");
 
-const users = path.join(__dirname, "../data/users.json");
+router.get("/", getUsers);
 
-router.get("/", (req, res, next) => {
-  fs.readFile(users, { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.send(data);
-  });
-});
+router.get("/:userId", getUser);
 
-router.get("/:id", (req, res, next) => {
-  fs.readFile(users, { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      next(err);
-      return;
-    }
-
-    const userList = JSON.parse(data);
-
-    const user = userList.find(
-      (selectedUser) => selectedUser._id === req.params.id
-    );
-
-    if (!user) {
-      res.status(404).send({ message: "ID do usuário não encontrado" });
-      return;
-    }
-
-    res.send(user);
-  });
-});
+router.post("/", createUser);
 
 module.exports = router;
