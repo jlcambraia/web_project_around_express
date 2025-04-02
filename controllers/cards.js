d3c36e340bc6c370c8829ec096ffc45c927da1bd
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-02 14:53:29 -0300

    feat: cria controllers para os cards

---
+++ controllers/cards.js
@@ -0,0 +1,21 @@
+const Card = require("../models/card");
+
+module.exports.getCards = (req, res, next) => {
+  Card.find({})
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => next(err));
+};
+
+module.exports.createCard = (req, res, next) => {
+  const { name, link } = req.body;
+
+  Card.create({ name, link })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => next(err));
+};
+
+module.exports.deleteCard = (req, res, next) => {
+  Card.findByIdAndDelete(req.params.id)
+    .then((card) => res.send({ data: card }))
+    .catch((err) => next(err));
+};
