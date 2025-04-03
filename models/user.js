b12ff9072953ecd196acbeea7de2c4908235e607
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-03 11:05:10 -0300

    feat: tira o versionamento dos usuários

--- models/user.js
+++ models/user.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlenght: 30,
-  },
-  about: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlenght: 30,
-  },
-  avatar: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => {
-        return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 30,
+    },
+    about: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 30,
+    },
+    avatar: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+        },
       },
     },
   },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("user", userSchema);
