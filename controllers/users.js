1f925d0f259b7258d3ac659b6e755233257ca74c
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-03 11:22:39 -0300

    feat: cria manipulação em caso de erro

--- controllers/users.js
+++ controllers/users.js
@@ -1,21 +1,49 @@
 const User = require("../models/user");
 
-module.exports.getUsers = (req, res, next) => {
+module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "Dados inválidos fornecidos" });
+      }
+      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+    });
 };
 
-module.exports.getUser = (req, res, next) => {
-  User.findById(req.params.id)
+module.exports.getUser = (req, res) => {
+  User.findById(req.params.userId)
+    .orFail(() => {
+      const error = new Error("Usuário não encontrado");
+      error.name = "DocumentNotFoundError";
+      throw error;
+    })
     .then((user) => res.send({ data: user }))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "CastError") {
+        return res.status(400).send({ message: "ID de usuário inválido" });
+      }
+      if (err.name === "DocumentNotFoundError") {
+        return res.status(404).send({ message: "Usuário não encontrado" });
+      }
+      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+    });
 };
 
-module.exports.createUser = (req, res, next) => {
+module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
-    .catch((err) => next(err));
+    .catch((err) => {
+      console.log(err);
+      if (err.name === "ValidationError") {
+        return res
+          .status(400)
+          .send({ message: "Dados inválidos fornecidos para criar usuário" });
+      }
+      return res.status(500).send({ message: "Ocorreu um erro no servidor" });
+    });
 };
