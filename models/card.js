fac697e32aa594560c39feed45823c63f7c03ad4
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-04-03 11:05:00 -0300

    feat: tira o versionamento dos cartões

--- models/card.js
+++ models/card.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 
-const cardSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 2,
-    maxlenght: 30,
-  },
-  link: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => {
-        return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+const cardSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlenght: 30,
+    },
+    link: {
+      type: String,
+      required: true,
+      validate: {
+        validator: (v) => {
+          return /(http:\/\/|https:\/\/)(www\.)?(.+)(\/)?(#)?/gi.test(v);
+        },
       },
     },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "user",
+      default: [],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    required: true,
-  },
-  likes: {
-    type: [mongoose.Schema.Types.ObjectId],
-    ref: "user",
-    default: [],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("card", cardSchema);
